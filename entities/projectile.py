f199f119c067425b4f87ea79a9d40e303fff0caf
axm <124406261+antichristcore@users.noreply.github.com>
2026-01-30 14:19:07 +0400

    Implement top-down shooter structure

---
+++ entities/projectile.py
@@ -0,0 +1,26 @@
+import arcade
+
+
+class Projectile:
+    def __init__(self, position, velocity, damage, radius, owner, knockback=80):
+        self.position = arcade.Vector(position[0], position[1])
+        self.velocity = arcade.Vector(velocity[0], velocity[1])
+        self.damage = damage
+        self.radius = radius
+        self.owner = owner
+        self.knockback = knockback
+        self.alive = True
+
+    def update(self, delta_time, arena_bounds):
+        self.position += self.velocity * delta_time
+        if (
+            self.position.x < arena_bounds[0]
+            or self.position.x > arena_bounds[2]
+            or self.position.y < arena_bounds[1]
+            or self.position.y > arena_bounds[3]
+        ):
+            self.alive = False
+
+    def draw(self):
+        color = arcade.color.CYAN if self.owner == "player" else arcade.color.SALMON
+        arcade.draw_circle_filled(self.position.x, self.position.y, self.radius, color)
